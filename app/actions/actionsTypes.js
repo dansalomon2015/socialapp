b489df05967d275abbef3b6b9f387c47bec86fcb
Salomon Nghukam <dsalomonj@gmail.com>
2022-12-21 09:55:56 +0100

    feature:signup - complete

--- app/actions/actionsTypes.js
+++ app/actions/actionsTypes.js
@@ -1,12 +1,12 @@
-const REQUEST = 'REQUEST'
-const SUCCESS = 'SUCCESS'
-const FAILURE = 'FAILURE'
-const defaultTypes = [REQUEST, SUCCESS, FAILURE]
+const REQUEST = 'REQUEST';
+const SUCCESS = 'SUCCESS';
+const FAILURE = 'FAILURE';
+const defaultTypes = [REQUEST, SUCCESS, FAILURE];
 
 function createRequestTypes(base, types = defaultTypes) {
-  const res = {}
-  types.forEach(type => (res[type] = `${ base }_${ type }`))
-  return res
+  const res = {};
+  types.forEach(type => (res[type] = `${base}_${type}`));
+  return res;
 }
 
 export const APP = createRequestTypes('APP', [
@@ -16,22 +16,23 @@ export const APP = createRequestTypes('APP', [
   'INIT_LOCAL_SETTINGS',
   'SET_MASTER_DETAIL',
   'SET_ROUTE',
-])
+]);
 export const APP_STATE = createRequestTypes('APP_STATE', [
   'FOREGROUND',
   'BACKGROUND',
   'INACTIVE',
-])
+]);
 
 // Login events
-export const LOGIN = createRequestTypes('LOGIN', [...defaultTypes, 'RESET'])
-export const USER = createRequestTypes('USER', ['SET'])
-export const LOGOUT = 'LOGOUT'
+export const LOGIN = createRequestTypes('LOGIN', [...defaultTypes, 'RESET']);
+export const USER = createRequestTypes('USER', ['SET']);
+export const LOGOUT = 'LOGOUT';
 export const CART = createRequestTypes('CART', [
   'ADD',
   'REMOVE',
   'CLEAR',
   'CREATE_ORDER',
   'UPDATE_ORDER',
-])
-export const CHAT = createRequestTypes('CHAT', ['FETCH_UNREAD', 'SET_UNREAD'])
+]);
+export const CHAT = createRequestTypes('CHAT', ['FETCH_UNREAD', 'SET_UNREAD']);
+export const SIGNUP = createRequestTypes('SIGNUP', [...defaultTypes, 'RESET']);
